a6599e4ed733e0984ef4356362daf32948601630
Ilya Kushlianski <kushliansky@gmail.com>
2017-11-10 05:43:14 +0300

    Change js behavior to handle dropdowns in main nav

--- js/script.js
+++ js/script.js
@@ -25,32 +25,75 @@
 
   $(document).ready(function(){
 
-    // menu toggle behavior
+    // mobile menu toggle behavior
     $('.toggle').on('click', function () {
       $('.nav-menu-div, .nav-group, #main-wrapper').toggleClass('toggled-on');
       $(this).toggleClass('fa-bars').toggleClass('fa-times');
     });
+    // large screen toggle menu behavior
+    $('.nav-group .toggle_largeScreens').on('click', function () {
+      $('.region-sitemap-nav').toggleClass('toggled-on');
+      // $('.region-sitemap-nav .toggle_largeScreens').toggleClass('fa-bars').toggleClass('fa-times');
+      if ( $('.region-sitemap-nav').hasClass('toggled-on') ) {
+        $('body').css({'overflow':'hidden'});
+      } else {
+        $('body').css({'overflow':'auto'});
+      }
+    });
+    $('.region-sitemap-nav .toggle_largeScreens').on('click', function () {
+      $('.region-sitemap-nav').toggleClass('toggled-on');
+      // $('.nav-group .toggle_largeScreens').toggleClass('fa-bars').toggleClass('fa-times');
+      if ( $('.region-sitemap-nav').hasClass('toggled-on') ) {
+        $('body').css({'overflow':'hidden'});
+      } else {
+        $('body').css({'overflow':'auto'});
+      }
+    });
 
+    if ( $(window).width() > 768 ) {
+      console.log('appended');
+      // $('.login-menu').detach();
+      $('.login-menu').appendTo('.login-menu-hider');
+    }
+
+    $(window).on("click", function(event){
+      if ($('.login-menu').hasClass('toggled-on') && event.target.className != "login-menu-hider") {
+        console.log(event);
+        $('.login-menu').removeClass('toggled-on');
+        event.stopPropagation();
+      }
+    });
+
+    $('.login-menu-hider').on('click', function(event){
+      if ( !$('.login-menu').hasClass('toggled-on') ) {
+        $('.login-menu').addClass('toggled-on');
+        event.stopPropagation();
+      }
+    });
 
     // hide menu on scroll down and show on scroll up
     var lastScrollTop = 0;
     $(window).scroll(function(event){
-      var st = $(this).scrollTop();
-      if (st > lastScrollTop && st > 100){
-        // downscroll code
-        if (!$('.nav-group').hasClass('toggled-on')) {$('.nav-group').fadeOut(100);}
-      } else {
-        // upscroll code
-        if (!$('.nav-group').hasClass('toggled-on')) {$('.nav-group').fadeIn(100);}
+      if ( $('.nav-group').css('position') == "fixed" ) {
+        var st = $(this).scrollTop();
+        if (st > lastScrollTop && st > 100){
+          // downscroll code
+          if (!$('.nav-group').hasClass('toggled-on')) {$('.nav-group').fadeOut(100);}
+        } else {
+          // upscroll code
+          if (!$('.nav-group').hasClass('toggled-on')) {$('.nav-group').fadeIn(100);}
+        }
+        lastScrollTop = st;
       }
-      lastScrollTop = st;
     });
 
 
     // move top-fixed menu under toolbar if user logged in
     if ($('body').hasClass('logged-in')) {
       var toolbarHeight = $('div.toolbar').height();
-      $(".nav-group").css({"top":toolbarHeight});
+      if ($(".nav-group").css('position') == 'fixed') {
+        $(".nav-group").css({"top":toolbarHeight});
+      }
       // $(".messages").css({"marginTop":toolbarHeight});
     }
 
