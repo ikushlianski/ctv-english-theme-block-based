0550a417032390dfed7e027b3119069e951efdd7
Ilya Kushlianski <kushliansky@gmail.com>
2017-11-27 15:56:15 +0300

    add js logic to make images 16:9

--- js/js_dev/16-9.js
+++ js/js_dev/16-9.js
@@ -13,13 +13,24 @@
     // make related news' images 16:9
     $('.block').each(function(){
       let thisBlockImgWidth = $(this).find('img').first().width();
+      let thisBlockImgHeight = $(this).find('img').first().height();
       $(this).find('img').each(function(item){
-        $(this).height(thisBlockImgWidth/16*9);
+        if ( !$(this).parents('.block').is("#block-views-latest-from-tourism-block") ) {
+          if ($(this).width() == thisBlockImgWidth) {
+            $(this).height(thisBlockImgWidth/16*9);
+          } else {
+            $(this).height($(this).width()/16*9);
+          }
+        } else {
+          let thisBlockImgW = $(this).parents(".block").find('img').first().width();
+          let thisBlockImgH = $(this).parents(".block").find('img').first().height();
+          $(this).height(thisBlockImgH);
+        }
       });
     });
 
     // make view items' images in taxonomy 16:9
-    $('.page-taxonomy-term .views-field-field-image').each(function(){
+    $('.page-taxonomy-term .view-taxonomy-term .views-field-field-image').each(function(){
       let thisViewImgWidth = $(this).find('img').first().width();
       $(this).find('img').each(function(item){
         $(this).height(thisViewImgWidth /16 * 9);
