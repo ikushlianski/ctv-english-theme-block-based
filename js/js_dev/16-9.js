bc0ee2ab2df6b5b5bb77b19acbd8bc2127d137f2
Ilya Kushlianski <kushliansky@gmail.com>
2017-12-03 17:14:28 +0300

    Add special js for carousels, exclude them from 16:9 script

--- js/js_dev/16-9.js
+++ js/js_dev/16-9.js
@@ -1,30 +1,43 @@
 (function ($, Drupal) {
 
-  // To understand behaviors, see https://drupal.org/node/756722#behaviors
-  Drupal.behaviors.my_custom_behavior = {
-    attach: function (context, settings) {
-
-      // Place your code here.
-
-    }
-  };
 
   $(document).ready(function(){
+
+    let blocksToExcludeFrom16_9 = [
+      "block-views-bc4d82ca41b3690bfc18d1f53c06d20c",
+      "block-views-editor-s-picks-block"
+    ];
+
     // make related news' images 16:9
     $('.block').each(function(){
-      let thisBlockImgWidth = $(this).find('img').first().width();
-      $(this).find('img').first().height(thisBlockImgWidth/16*9);
-      let thisBlockImgHeight = $(this).find('img').first().height();
-      $(this).find('img').each(function(item){
-        if ($(this).width() == thisBlockImgWidth) {
-          $(this).height(thisBlockImgWidth/16*9);
-        } else {
-          // if ( !$(this).parents('.view').hasClass("carouselView") ) {
+
+      let currentElemId = $(this).attr("id");
+
+      if ( ! blocksToExcludeFrom16_9.includes(currentElemId) ) {
+        let thisBlockImgWidth = $(this).find('img').first().width();
+        $(this).find('img').first().height(thisBlockImgWidth/16*9);
+        let thisBlockImgHeight = $(this).find('img').first().height();
+
+        $(this).find('img').each(function(item){
+          if ($(this).width() == thisBlockImgWidth) {
+            $(this).height(thisBlockImgWidth/16*9);
+          } else {
             $(this).width(thisBlockImgWidth);
             $(this).height($(this).width()/16*9);
-          // }
-        }
-      });
+          }
+        });
+      } else {
+        let thisBlockImgWidth = $(this).find('img').first().width();
+        let thisBlockImgHeight = $(this).find('img').first().height();
+        $(this).find('img').each(function(item){
+          if ($(this).width() == thisBlockImgWidth) {
+            $(this).height(thisBlockImgWidth/16*12);
+          } else {
+            $(this).width(thisBlockImgWidth);
+            $(this).height($(this).width()/16*12);
+          }
+        });
+      }
     });
 
     // make view items' images in taxonomy 16:9
