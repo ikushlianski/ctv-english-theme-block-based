6170d005693cb99621d86abbd5468dce09de96ad
Ilya Kushlianski <kushliansky@gmail.com>
2017-12-05 03:49:46 +0300

    Make basic English footer

--- templates/regions/region--footer.tpl.php
+++ templates/regions/region--footer.tpl.php
@@ -7,24 +7,29 @@
  * @see https://drupal.org/node/1728140
  */
 ?>
-<?php if ($content): ?>
+
   <footer class="footer <?php print $classes; ?>" role="contentinfo">
     <div class="footer-content">
-      <div class="footer-nav-menu">
-        <?php $footer_menu = menu_navigation_links('menu-main-menu-2'); ?>
-        <?php print theme('links__menu_main_menu_2', array('links' => $footer_menu, 'attributes' => array('id' => 'main-menu-footer', 'class' => array('main-menu links', 'clearfix')))); ?>
-      </div>
+
+      <?php $footer_menu = menu_navigation_links('main-menu'); ?>
+      <?php print theme('links__menu_main_footer', array('links' => $footer_menu, 'attributes' => array('id' => 'main-menu-footer', 'class' => array('main-menu-footer links', 'clearfix')))); ?>
+
       <div class="other-footer-content-wrapper">
         <div class="site-brand-footer">
           <div class="subtext">Belarusian Television Channel CTV</div>
           <div class="company-name">Stolichnoe Televidenie</div>
         </div>
-        <div class="other-footer-links-wrapper">
-          <nav class="other-footer-links">
 
-          </nav>
-        </div>
+        <nav class="other-footer-links-wrapper">
+          <ul class="other-footer-links links">
+            <li class="footer-link"><a href="#">Top management</a></li>
+            <li class="footer-link"><a href="#">Contact us</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="site-author">
+        Site created by&nbsp;<a href="ilya.online">Ilya Kushlianski</a>
       </div>
     </div>
   </footer>
-<?php endif; ?>
