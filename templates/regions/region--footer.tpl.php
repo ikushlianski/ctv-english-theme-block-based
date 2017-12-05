c1be921c3ea0ae065ad907aa857f70d5a8124169
Ilya Kushlianski <kushliansky@gmail.com>
2017-12-05 23:55:02 +0300

    Clone main menu into footer, fix footer positioning of logo

--- templates/regions/region--footer.tpl.php
+++ templates/regions/region--footer.tpl.php
@@ -11,14 +11,23 @@
   <footer class="footer <?php print $classes; ?>" role="contentinfo">
     <div class="footer-content">
 
-      <?php $footer_menu = menu_navigation_links('main-menu'); ?>
+      <?php $footer_menu = menu_navigation_links('menu-footer-menu'); ?>
       <?php print theme('links__menu_main_footer', array('links' => $footer_menu, 'attributes' => array('id' => 'main-menu-footer', 'class' => array('main-menu-footer links', 'clearfix')))); ?>
 
 
       <div class="other-footer-content-wrapper">
-        <div class="site-brand-footer">
-          <div class="subtext">Belarusian Television Channel CTV</div>
-          <div class="company-name">Stolichnoe Televidenie</div>
+
+        <div class="logo-and-branding">
+          <div class="site-logo-footer">
+            <a href="/" title="<?php print t('Home'); ?>" rel="home"  id="logo_largeScreens_footer" class="logo_largeScreens_footer">
+              <img src="<?php echo file_create_url(path_to_theme().'/images-source/logo_stv_footer.png'); ?>" alt="<?php print t('Home'); ?>" />
+            </a>
+          </div>
+        
+          <div class="site-brand-footer">
+            <div class="subtext">Belarusian Television Channel CTV</div>
+            <div class="company-name">Stolichnoe Televidenie</div>
+          </div>
         </div>
 
         <nav class="other-footer-links-wrapper">
@@ -28,13 +37,6 @@
           </ul>
         </nav>
       </div>
-
-      <div class="site-logo-footer">
-        <a href="/" title="<?php print t('Home'); ?>" rel="home"  id="logo_largeScreens_footer" class="logo_largeScreens_footer">
-          <img src="<?php echo file_create_url(path_to_theme().'/images-source/logo_stv_footer.png'); ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      </div>
-
 
       <div class="site-author">
         Site created by&nbsp;<a href="ilya.online">Ilya Kushlianski</a>
